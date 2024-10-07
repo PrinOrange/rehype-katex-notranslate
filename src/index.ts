496aafabbae7078888f88cbbdecc369beab9927b
PrinOrange <prinorange@outlook.com>
2024-10-07 16:09:32 +0800

    [fix] rewrite the visiting process and ts compiling options

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import type {Element, Node, Root} from 'hast'
-import {selectAll} from 'hast-util-select'
-import type {Plugin} from 'unified'
+import {Element} from 'hast'
+import {Plugin} from 'unified'
+import {visit} from 'unist-util-visit'
 
 const rehypeKatexNoTranslate: Plugin = () => {
-  return (tree: Node) => {
-    if ((tree as Root).children) {
-      const root = tree as Root
-      const katexNodes = selectAll('span.katex', root) as Element[]
-      katexNodes.forEach((node: Element) => {
+  return (tree) => {
+    visit(tree, 'element', (node: Element) => {
+      if (node.tagName !== 'span') return
+      const className = node.properties?.className
+      if (!className) return
+      if (
+        Array.isArray(className) &&
+        (className.includes('katex-display') || className.includes('katex'))
+      ) {
         node.properties.translate = 'no'
-      })
-    }
+      }
+    })
   }
 }
 
