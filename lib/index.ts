519c3ad37d098fef857ce1656c7ffda52ded2020
PrinOrange <prinorange@outlook.com>
2024-10-20 12:16:20 +0800

    fix(format): fix code formats

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,21 @@
-import type {Root} from 'hast'
-import type {Plugin} from 'unified'
-import {visit} from 'unist-util-visit'
+import type {Root} from 'hast';
+import type {Plugin} from 'unified';
+import {visit} from 'unist-util-visit';
 
 const rehypeKatexNoTranslate: Plugin<[], Root> = () => {
   return (tree: Root) => {
     visit(tree, 'element', (node) => {
-      if (node.tagName !== 'span') return
-      const className = node.properties?.className
-      if (!className) return
+      if (node.tagName !== 'span') return;
+      const className = node.properties?.className;
+      if (!className) return;
       if (
         Array.isArray(className) &&
         (className.includes('katex-display') || className.includes('katex'))
       ) {
-        node.properties.translate = 'no'
+        node.properties.translate = 'no';
       }
-    })
-  }
-}
+    });
+  };
+};
 
-export default rehypeKatexNoTranslate
+export default rehypeKatexNoTranslate;
